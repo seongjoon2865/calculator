c3184779e6dd3682abf694830c576c8cb2c841d9
seongjoon2865 <cusk09@gmail.com>
2025-01-09 12:54:04 +0900

    Delete calculator directory

--- calculator/src/main/java/com/example/calculator/ArithmeticCalculator.java
+++
@@ -1,9 +0,0 @@
-package com.example.calculator;
-
-public class ArithmeticCalculator {
-
-    public static Double calculate(Number operand1, Number operand2, String operatorSymbol) {
-        Operator operator = Operator.fromSymbol(operatorSymbol);
-        return operator.apply(operand1.doubleValue(), operand2.doubleValue());
-    }
-}
